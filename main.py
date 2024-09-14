f818bb95290bf180f18d63b8d84dd73c072abe76
DenisValim <denis_zv@protonmail.com>
2024-09-14 16:09:11 -0300

    final

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from datetime import time
+import time
 import numpy as np
 import pandas as pd
 import streamlit as st
 from annotated_text import annotated_text
+
 st.set_page_config(
     page_title="Denis Zorzetti Valim",
     page_icon="🧊",
@@ -15,29 +16,36 @@ st.set_page_config(
     }
 )
 
-st.subheader("Seja bem vindo!")
-st.write("Esse site tem como objetivo mostrar meu conhecimento em desenvovimento de WebApps em Python.")
+st.subheader("Seja bem-vindo!")
+st.write("Esse site tem como objetivo mostrar meu conhecimento em desenvolvimento de WebApps em Python.")
 st.subheader('Principais skills')
+
 annotated_text(
     ("Digital Solutions", " "),
-    " | ", ("Desenvolvedor Python ",""),
-    " | ",("Analista de Dados  ","*"),
-    " | ",("Automação  ", " "),
-    " | ",("Desenvolvedor Blockchain", " "),
+    " | ", ("Desenvolvedor Python ", ""),
+    " | ", ("Analista de Dados  ", "*"),
+    " | ", ("Automação  ", " "),
+    " | ", ("Desenvolvedor Blockchain", " "),
     "."
 )
+
 def stream_data():
     # Gerar palavras uma por uma com delay
     for word in texto_resumo.split(" "):
         yield word + " "
-        time.sleep(0.02)
+        time.sleep(0.1)
 
 texto_resumo = """
 Profissional com grande aptidão em desenvolvimento, com foco na solução de problemas de negócios, experiência de mais de 18 anos no ramo automobilístico e toda cadeia logística. Conhecedor de diversas linguagens de programação: Python, SQL, HTML, RPA, JavaScript, VBA, Solidity, Node.js e JSON. Capacitado em análise de dados e desenvolvimento de automação de relatórios e dashboards, com conhecimento em programação de Smart Contracts na rede Ethereum. Habilidades avançadas em softwares corporativos (SAP - MM, SD e WM, formação ABAP na academia SAP), desenvolvimento de scripts via VBA, Excel Avançado e Python, utilização de ferramentas de business intelligence (Power BI, QLink), GKO (pagamentos de fretes) e SharePoint. Atualmente me dedico a adquirir novos conhecimentos na tecnologia de blockchain voltada ao setor logístico.
 """
+
 st.subheader('Resumo profissional')
-with st.container(border=True):
-    st.write(stream_data())
 
+# Usar placeholder para atualizar o conteúdo conforme o stream_data gera texto
+placeholder = st.empty()
 
-
+with st.container():
+    texto_completo = ""
+    for parte in stream_data():
+        texto_completo += parte
+        placeholder.write(texto_completo)
